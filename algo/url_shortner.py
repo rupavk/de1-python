3b7baf63e59addcf28f85f0abddafdae55818387
rupavk <106325862+rupavk@users.noreply.github.com>
2024-03-08 16:16:21 +0000

    Update url_shortner.py

--- algo/url_shortner.py
+++ algo/url_shortner.py
@@ -1,7 +1,12 @@
 import random
 import string
 
+function shorterUrl(url):
+            storeinDB()
+return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
 
+function 
+    
 if __name__ == '__main__':
     pass
 
