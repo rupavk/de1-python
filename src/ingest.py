910f7ae2101e71b4edc649e7af876320057aac13
rupavk <106325862+rupavk@users.noreply.github.com>
2024-03-08 17:08:03 +0000

    Update ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,9 +1,11 @@
 import json
 import sys
+import pandas as pd
+import pandas.dataframe as dataframe
 
 try:
     with open(sys.argv[1]) as data:
-        for line in data:
-            print(line)
+     df= pd.read.json("/resources/mock_data.json")
+       return df.filter[df[location_id[1:5]]
 except FileNotFoundError:
     print("File not found, please check your path")
