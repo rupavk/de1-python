13ed390e2866b678a8c6176765f38dea74f9c1da
rupavk <106325862+rupavk@users.noreply.github.com>
2024-03-08 16:35:20 +0000

    Update url_shortner.py

--- algo/url_shortner.py
+++ algo/url_shortner.py
@@ -1,11 +1,21 @@
 import random
 import string
-
+dictinary1,dictionary2
 function shorterUrl(url):
-            storeinDB()
-return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+       if(dictionary[url]):
+            return dictionary[url]
+else:
+     dictionary1.Add(url,''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8)))
+     dictionary2.Add(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8)),url)
+      return dictionary[url],
 
-function 
+
+function restoreurl(short)
+  if (dictionary2(short)):
+    return dictionary[short]
+  else 
+    return null
+
     
 if __name__ == '__main__':
     pass
